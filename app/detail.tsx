ff77522815ff3d387854426b7ef8ac73c7363afe
Bas De Meurichy <s148791@ap.be>
2025-02-19 11:58:30 +0100

    Modify detail.tsx to get id from route params to filter sightings on id.

--- app/detail.tsx
+++ app/detail.tsx
@@ -1,6 +1,14 @@
+import SightingDetail from "@/components/SightingOverview";
+import { useSightings } from "@/Providers/Sightings";
+import { useLocalSearchParams } from "expo-router";
 import { View,Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function Index() {
+  const {sightings} = useSightings();
+  const params = useLocalSearchParams<{ id:string }>();
+  const numericId = parseInt(params.id,10);
+  const details = sightings.filter(sighting => sighting.id === numericId)
   return (
     <View
       style={{
@@ -9,7 +17,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-extrabold">Detail works!</Text>
+        <FlatList
+                  className="self-center"
+                  data={details}
+                  renderItem={({ item }) => (
+                <SightingDetail sighting={item} />
+                  )}
+                />
     </View>
   );
 }
