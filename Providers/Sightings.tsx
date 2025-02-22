177d87b52eabf7f75701cf3369c5b5edb97a668c
BasDM <bas.demeurichy@gmail.com>
2025-02-22 14:13:21 +0100

    Refactor Sightings to use async await instead of mix between promises + then and async await.

--- Providers/Sightings.tsx
+++ Providers/Sightings.tsx
@@ -1,69 +1,82 @@
 import { Sighting } from "@/types";
-import React, { createContext, useContext, useState, useEffect, ReactElement } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactElement,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface SightingsContextType {
-    sightings: Sighting[];
-    addSighting: (sighting: Sighting) => Promise<Sighting>;
+  sightings: Sighting[];
+  addSighting: (sighting: Sighting) => Promise<Sighting>;
 }
 
 const SightingsContext = createContext<SightingsContextType>({
-    sightings: [],
-    addSighting: async (sighting: Sighting) => sighting
+  sightings: [],
+  addSighting: async (sighting: Sighting) => sighting,
 });
 
-export const SightingsProvider = ({ children } : { children: ReactElement }) => {
-    const [sightings, setSightings] = useState<Sighting[]>([]);
+export const SightingsProvider = ({ children }: { children: ReactElement }) => {
+  const [sightings, setSightings] = useState<Sighting[]>([]);
 
-    // Get sightings and store them in AsyncStorage
-    function initSightings() {
-        fetch("https://sampleapis.assimilate.be/ufo/sightings")
-            .then((response) => response.json())
-            .then(async (sightings) => {
-                AsyncStorage.setItem(
-                    '@app:sightings',
-                    JSON.stringify(sightings)
-                )
-                    .then(() =>
-                        setSightings(sightings)
-                    )
-                    .catch((error) => console.error('Error setting sightings:', error));
-            });
+  // Get sightings and store them in AsyncStorage
+  async function initSightings() {
+    try {
+      const response = await fetch(
+        "https://sampleapis.assimilate.be/ufo/sightings"
+      );
+      const sightings = await response.json();
+      await AsyncStorage.setItem("@app:sightings", JSON.stringify(sightings));
+      setSightings(sightings);
+    } catch (error) {
+      console.error("Error setting sightings:", error);
     }
+  }
 
-    // Add sighting
-    async function addSighting(sighting: Sighting): Promise<Sighting> {
-        AsyncStorage.getItem('@app:sightings')
-            .then((storedData) => {
-                const sightingsList: Sighting[] = storedData ? JSON.parse(storedData) : [];
-                const updatedSightings = [...sightingsList, sighting];
-                AsyncStorage.setItem('@app:sightings', JSON.stringify(updatedSightings))
-                    .then(() => setSightings(updatedSightings))
-                    .catch((error) => console.error('Error adding sighting:', error));
-            })
-            .catch((error) => console.error('Error adding sighting:', error));
-        return sighting;
+  // Add sighting
+  async function addSighting(sighting: Sighting): Promise<Sighting> {
+    try {
+      const storedData = await AsyncStorage.getItem("@app:sightings");
+      const sightingsList: Sighting[] = storedData
+        ? JSON.parse(storedData)
+        : [];
+      const updatedSightings = [...sightingsList, sighting];
+      await AsyncStorage.setItem(
+        "@app:sightings",
+        JSON.stringify(updatedSightings)
+      );
+      setSightings(updatedSightings);
+    } catch (error) {
+      console.error("Error adding sighting:", error);
     }
+    return sighting;
+  }
 
-    // Get sightings only initially
-    useEffect(() => {
-        AsyncStorage.getItem('@app:sightings')
-            .then((storedData) => {
-                if (storedData) {
-                    setSightings(JSON.parse(storedData));
-                } else {
-                    initSightings();
-                }
-            })
-    }, []);
+  // Get sightings only initially
+  useEffect(() => {
+    async function fetchSightings() {
+      try {
+        const storedData = await AsyncStorage.getItem("@app:sightings");
+        if (storedData) {
+          setSightings(JSON.parse(storedData));
+        } else {
+          await initSightings();
+        }
+      } catch (error) {
+        console.error("Error fetching sightings:", error);
+      }
+    }
+    fetchSightings();
+  }, []);
 
-    // Add Context Provider for all children
-    return (
-        <SightingsContext.Provider value={{ sightings, addSighting }}>
-            {children}
-        </SightingsContext.Provider>
-    )
+  // Add Context Provider for all children
+  return (
+    <SightingsContext.Provider value={{ sightings, addSighting }}>
+      {children}
+    </SightingsContext.Provider>
+  );
 };
 
 export const useSightings = () => useContext(SightingsContext);
-
