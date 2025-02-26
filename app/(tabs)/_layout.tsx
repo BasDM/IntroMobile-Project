9d64a052f968711a7505bfafa422d75e4b65f5f8
BasDM <bas.demeurichy@gmail.com>
2025-02-26 20:31:51 +0100

    Add different icons to navbar using expo vector-icons.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,22 @@
 import { Tabs } from "expo-router";
 import "../../global.css"; 
+import { Ionicons } from '@expo/vector-icons'
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="map" options={{ title: "Map" }} />
-      <Tabs.Screen name="add" options={{ title: "Add" }} />
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+        )}} 
+      />
+      <Tabs.Screen name="map" options={{ title: "Home", tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map" color={color} size={size} />
+        )}} 
+      />
+      <Tabs.Screen name="add" options={{ title: "Home", tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" color={color} size={size} />
+        )}} 
+      />
     </Tabs>
   );
 }
