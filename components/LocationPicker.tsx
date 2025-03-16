86daec06c38a0eb48547992019af77d56c00a8ed
warre002 <warre.bossaert@gmail.com>
2025-03-16 16:10:29 +0100

    Supressing component for location picker

---
+++ components/LocationPicker.tsx
@@ -0,0 +1,6 @@
+import { Sighting } from "@/types";
+
+// Suppresses error when importing Map component in other files
+export default function LocationPicker({ sighting, setSighting }: { sighting: Sighting, setSighting: Function }) {
+    return <></>
+}
